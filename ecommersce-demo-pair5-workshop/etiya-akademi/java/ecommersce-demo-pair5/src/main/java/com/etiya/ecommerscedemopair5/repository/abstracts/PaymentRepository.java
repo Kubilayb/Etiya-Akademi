60a75aff260ee3387ca480165ca2e91c93a31ccb
BeratKubilay <109307078+Kubilayb@users.noreply.github.com>
2022-12-14 16:42:34 +0300

    Delete ecommersce-demo-pair5-workshop/etiya-akademi/java/ecommersce-demo-pair5/src/main/java/com/etiya/ecommerscedemopair5 directory

--- ecommersce-demo-pair5-workshop/etiya-akademi/java/ecommersce-demo-pair5/src/main/java/com/etiya/ecommerscedemopair5/repository/abstracts/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etiya.ecommerscedemopair5.repository.abstracts;
-
-import com.etiya.ecommerscedemopair5.entities.concretes.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment,Integer> {
-}
